bee8ba71f0ffb99ea73352e391c202c6790238d9
dbalay <daadbenalaya76@gmail.com>
2023-11-18 10:54:40 +0100

    first commit

---
+++ src/Components/MovieList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+
+function MovieList ({props}) {
+  console.log(props)
+
+    return (
+        <div className='items-movie'>
+            { props&&props.map((el)=>  <MovieCard props={el}   />)   }
+
+        </div>
+      )
+}    
+ 
+
+export default MovieList
